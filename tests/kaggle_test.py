57c82c8b7c5830b256a853963cc98428c7ca9f65
RSM-git <74561311+RSM-git@users.noreply.github.com>
2023-01-16 16:44:16 +0100

    test 2

--- tests/kaggle_test.py
+++ tests/kaggle_test.py
@@ -3,6 +3,11 @@ import os
 
 os.makedirs("data/raw", exist_ok=True)
 
+if os.environ.get("KAGGLE_USRENAME") is not None:
+    print("Got Kaggle username")
+
+if os.environ.get("KAGGLE_KEY") is not None:
+    print("Got Kaggle key")
 
 def test_download():
     kaggle.api.dataset_download_file("clmentbisaillon/fake-and-real-news-dataset",
